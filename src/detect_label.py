6f4e4f1263010ac4f87aa31ef852114cca0a96d5
josephdiniso <josephdiniso@gmail.com>
2021-05-26 21:18:56 -0400

    Cleaned up code and file structure

--- src/detect_label.py
+++ src/detect_label.py
@@ -5,7 +5,7 @@ import numpy as np
 import cv2
 import webbrowser
 
-from create_label import analyze_bar
+from b64_to_bar import bar_to_b64
 
 
 def analyze_contours(frame: np.array, contours: List) -> List:
@@ -56,6 +56,62 @@ def get_webcam_values():
             break
     cap.release()
     cv2.destroyAllWindows()
+    return b64_values
+
+
+def calculate_ratios(heights: List[float]) -> List[int]:
+    """
+    Given a list of floats representing the height of the bars, these values are converted to their
+    original base-64 encoding.
+    Args:
+        scale (float): Maximum height of a data bar
+        heights (List[float]): List of heights of bars
+
+    Returns:
+        List[str]: List of b64 encoded characters
+    """
+    ratios = []
+    for height in heights:
+        ratio = height / max(heights)
+        val = round(ratio * 8) - 1
+        ratios.append(int(val))
+    b64_values = bar_to_b64(ratios)
+    return b64_values
+
+
+def analyze_bar(src: np.array) -> List[str]:
+    """
+    Receives an image of a embedded QR-code. Analyzes the bars in the image to retrieve the b64 data
+    encoded in it
+    Args:
+        src (np.array): Input image
+
+    Returns:
+        List[int] List of b64 characters converted from the image
+    """
+    # Detecting Bar's height
+    src = cv2.resize(src, (300, 150))
+    # Cuts edges of image
+    src = src[10:-10, 10:-10]
+    thresh = cv2.adaptiveThreshold(src, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 9, 5)
+    contours, h = cv2.findContours(thresh, 1, 2)
+    bar_heights = []
+    img_height, img_width = src.shape
+    x_vals = []
+    for cnt in contours:
+        top = (cnt[cnt[:, :, 1].argmin()])[0][1]
+        bottom = (cnt[cnt[:, :, 1].argmax()])[0][1]
+        x = (cnt[cnt[:, :, 0].argmax()][0])[0]
+        height = round((bottom - top) / img_height, 5)
+        bar_heights.append(height)
+        # This is stored so that we can sort bar heights based on their respective x value
+        x_vals.append(x)
+    # This sorts the bar heights based on their respective x position as the contours do not
+    # automatically sort from left to right
+    sorted_bars = zip(x_vals, bar_heights)
+    sorted_bars = sorted(sorted_bars)
+    sorted_bars_final = [bar for _, bar in sorted_bars]
+    b64_values = calculate_ratios(sorted_bars_final[1:-3])
     return b64_values
 
 
